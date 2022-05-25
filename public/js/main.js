920ed980b96fe7c35ba823e574bd1a237c12bc6a
Siddhi Kakkar <siddhikakkar072gmail.com>
2022-05-25 23:39:24 +0530

    nodejs project

---
+++ public/js/main.js
@@ -0,0 +1,67 @@
+const cityName = document.getElementById('cityName');
+const submitBtn = document.getElementById('submitBtn');
+const city_name = document.getElementById('city_name');
+const temp_real_val = document.getElementById('temp_real_val');
+const temp_status = document.getElementById('temp_status');
+const datahide = document.querySelector('.middle_layer');
+
+const getInfo = async(event) => {
+    event.preventDefault();
+    let cityVal = cityName.value;
+    if(cityVal === ""){
+        city_name.innerText = `Invalid Input`;
+        datahide.classList.add('data_hide');
+    }
+    else{
+        try{
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=f1ed3b58e05437af6be679f4ee283ea0`;
+            const response = await fetch(url);
+            const data = await response.json();
+            const arrData = [data];
+            city_name.innerText = `${arrData[0].name}, ${arrData[0].sys.country}`;
+            temp_real_val.innerText = arrData[0].main.temp;
+            temp.innerText = arrData[0].main.temp;
+            const tempMood = arrData[0].weather[0].main;
+
+            // condition to check sunny or cloudy
+            if(tempMood == "Clear"){
+                temp_status.innerHTML = 
+                    "<i class='fas fa-sun' style='color:#eccc68;'></i>";
+            }else if(tempMood == "Clouds"){
+                temp_status.innerHTML = 
+                "<i class='fas fa-cloud' style='color:#f1f2f6;'></i>";
+            }else if(tempMood == "Rain"){
+                temp_status.innerHTML = 
+                "<i class='fas fa-rain' style='color:#a4b0be;'></i>";
+            }else{
+                temp_status.innerHTML = 
+                "<i class='fas fa-cloud' style='color:#f1f2f6;'></i>";
+            }
+            datahide.classList.remove('data_hide');
+        }
+        catch{
+            city_name.innerText = `Please enter a valid city`;
+            datahide.classList.add('data_hide');
+        }
+    }
+}
+
+submitBtn.addEventListener('click', getInfo);
+
+// const submitBtn = document.getElementById('submitBtn');
+// const cityName = document.getElementById('cityName');
+
+// const getInfo = async(event) =>{
+//     event.preventDefault();
+//     // let url = `https://api.openweathermap.org/data/2.5/weather?q=Dehradun&units=metric&appid=f1ed3b58e05437af6be679f4ee283ea0`;
+//     let cityVal = cityName.value;
+//     if(){
+
+//     }
+//     else{
+//         let url = `https://api.openweathermap.org/data/2.5/weather?q=Dehradun&units=metric&appid=f1ed3b58e05437af6be679f4ee283ea0`;
+//         const response = await fetch(url);
+//     }
+// }
+
+// submitBtn.addEventListener('click',getInfo);
